ee2c1b147bfe3a3773fc60e1c89b9bcfdb90caa0
Pavlove BIOKOU <pbiokou@gmail.com>
2023-11-07 21:03:59 +0100

    WIP

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index () {
+        return [
+            [
+                'title' => 'Ici mon super article',
+                'content' => 'Ici mon contenu'
+            ]
+        ];
+    }
+
+    public function show ($slug, $id) {
+        return 'Ici mon super article avec comme slug : ' . $slug . ' avec l\'ID ' . $id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', App\Http\Controllers\WelcomeController::class);
 
-Route::prefix('/articles')->name('posts.')->group(function () {
+Route::prefix('/articles')
+    ->name('posts.')
+    ->controller(App\Http\Controllers\PostController::class)
+    ->group(function () {
 
-    Route::get('/', function (Request $request) {
-        return [
-            [
-                'title' => 'Ici mon super article',
-                'content' => 'Ici mon contenu'
-            ]
-        ];
-    })->name('index');
+        Route::get('/', 'index')->name('index');
 
-    Route::get('/{slug}-{id}', function ($slug, $id) {
-        return 'Ici mon super article avec comme slug : ' . $slug . ' avec l\'ID ' . $id;
-    })
-        ->where([
-        'slug' => '[a-z0-9\-]+',
-        'id' => '[0-9]+'
-    ])->name('show');
-});
+        Route::get('/{slug}-{id}', 'show')
+            ->where([
+                'slug' => '[a-z0-9\-]+',
+                'id' => '[0-9]+'
+            ])->name('show');
+    });
 
 
 
