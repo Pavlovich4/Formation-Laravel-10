ce8a8e3fee32b262da4d8833eb8c3195e1cda04c
Pavlove BIOKOU <pbiokou@gmail.com>
2024-03-21 18:43:09 +0100

    Eager loading et Policies

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,7 +11,6 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd(auth()->user()->posts);
         return view('welcome');
     }
 }
